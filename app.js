3178b03c95c519921092c85e9422aa117c2fcbda
Vince Sangkhae <vsangk@gmail.com>
2020-05-06 11:33:58 -0700

    Add UserArticle PUT endpoint
    * Add CORS middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 const { sequelize } = require('./models');
 
 const indexRouter = require('./routes/index');
@@ -27,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { Feed, User } = require('../models');
+const { Feed, User, UserArticle } = require('../models');
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -30,6 +30,18 @@ router.get('/:userId/articles', async function (req, res, next) {
   const user = await User.findByPk(req.params.userId);
   const feeds = await user.getArticles();
   res.json(feeds);
+});
+
+router.put('/:userId/articles/:articleId', async function (req, res, next) {
+  const userArticle = await UserArticle.findOne({
+    where: {
+      userId: req.params.userId,
+      articleId: req.params.articleId,
+    },
+  });
+
+  const updated = await userArticle.update({ hasRead: req.body.hasRead });
+  res.json(updated);
 });
 
 module.exports = router;
